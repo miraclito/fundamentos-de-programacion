d65f06f4eac672864b0d2be2fbe7e52c2152bb65
miraclito <hiramjeremi.hairado@gmail.com>
2022-04-21 17:18:43 -0500

    miracle

---
+++ micausayatoro.py
@@ -0,0 +1,20 @@
+# Problema 1.1
+#intercambio de valores 
+
+a: int
+b: int
+t: int
+a = input("ingrese el valor de a:")
+b = input("ingrese el valor de b:")
+
+t = a
+a = b
+b = t
+
+
+print("el valor de a:",a)
+print("el valor de b:",b)
+print("el valor de t:",t)
+
+
+
